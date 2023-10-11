58a75deb1ab5989a6499c6b5a597345d8dc31a69
laluhairumbaki <laluhairumbaki@gmail.com>
2023-10-11 11:58:34 +0800

    11/10/2023

---
+++ 01-pengenalanclassdanobjek/src/com/tutorial/Main.java
@@ -0,0 +1,35 @@
+package com.tutorial;
+
+class mahasiswa {
+    String nama;
+    String NIM;
+    String prodi;
+}
+
+
+public class Main {
+     public static void main(String[] args) {
+// instansial pembuatan projek
+mahasiswa mhs1 =new mahasiswa();
+
+// objek mhs1 diberi atribut
+mhs1.nama ="baki";
+mhs1.NIM ="22241024";
+mhs1.prodi ="pti";
+
+System.out.println("nama mahasiswa :" + mhs1.nama);
+System.out.println("NIM : " + mhs1.prodi);
+System.out.println("program studi :" + mhs1.prodi);
+    
+
+
+
+
+}
+
+   
+
+}
+    
+
+   
